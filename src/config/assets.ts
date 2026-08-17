eaec4eb1ed66e9235baa4c72724e539076890e51
karolhuzarski-maker <karol.huzarski@gmail.com>
2026-08-17 10:02:17 +0200

    Improve responsive header and use brand logo

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -2,10 +2,12 @@ import brain from '../../brain.png'
 import raw from '../../1 raw.png'
 import groundTruth from '../../2 gt.png'
 import output from '../../3 output.png'
+import logoWhite from '../../LOGO  white.png'
 
 export const assets = {
   brain,
   raw,
   groundTruth,
   output,
+  logoWhite,
 } as const
